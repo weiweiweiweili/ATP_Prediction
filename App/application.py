2063fd3e43f786a393e2c784fa96cd8def0ee620
wei li <xiaoweili2018@u.northwestern.edu>
2018-02-26 18:15:51 -0600

    data update

--- App/application.py
+++ App/application.py
@@ -1,6 +1,8 @@
-from flask import render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash
 from msiapp import application, db
 from msiapp.models import Track
+
+from sklearn.externals import joblib
 
 
 @application.route('/')
@@ -16,19 +18,23 @@ def index():
 
     return render_template('index.html', tracks=Track.query.all())
 
-@application.route('/add', methods=['POST'])
-def add_entry():
-    """View that process a POST with new song input
+# @application.route('/add', methods=['POST'])
+# def add_entry():
+#     """View that process a POST with new song input
 
-    :return: redirect to index page
-    """
+#     :return: redirect to index page
+#     """
 
-    track1 = Track(artist=request.form['artist'], album=request.form['album'], title=request.form['title'])
-    db.session.add(track1)
-    db.session.commit()
+#     track1 = Track(artist=request.form['artist'], album=request.form['album'], title=request.form['title'])
+#     db.session.add(track1)
+#     db.session.commit()
 
-    return redirect(url_for('index'))
-
+#     return redirect(url_for('index'))
+@app.route('/predict', methods=['POST'])
+def make_prediction():
+    if request.method=='POST':
+        return render_template('index.html', label="3")
 
 if __name__ == "__main__":
-    application.run(debug=True)
+    model = joblib.load('model.pkl')
+    application.run(host=‘0.0.0.0’,debug=True)
